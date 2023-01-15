26161487c713fd58b34c5cfb838496c05bb8b8de
GitRockie <elromankos@gmail.com>
2023-01-15 15:18:04 +0100

    Farms pages Header
    Creating Scaffold
    Wrapping body with Column, positioning of the Widgets
    Icon search create and style from AppColors static variable

--- lib/home/main_farms_page.dart
+++ lib/home/main_farms_page.dart
@@ -1,3 +1,4 @@
+import 'package:farmee_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class MainFarmsPage extends StatelessWidget {
@@ -5,25 +6,45 @@ class MainFarmsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: Row(
-          children: [
-            Column(
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          //Scrolling parameter goes here
+          child: Container(
+            margin: const EdgeInsetsDirectional.only(top: 50, bottom: 15),
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
               children: [
-                Text('Country'),
-                Text('City'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: const [
+                        Text('Country'),
+                        Text('City'),
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: AppColors.mainColor),
+                        child: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
-            Container(
-              width: 45,
-              height: 45,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15), color: Colors.blue),
-            )
-          ],
-        ),
-      ),
-    );
+          ),
+        )
+      ],
+    ));
   }
 }
