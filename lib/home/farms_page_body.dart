b5a2623d7a4c6e76a9899ef4af135106bfe986ae
GitRockie <elromankos@gmail.com>
2023-01-16 15:33:33 +0100

    Scale Up and Down in PageView
    _currentPageValue declaring
    From PageController we access the Page property
    Overriding init state attaching a listener
    SetState to update page value
    Ovveride to dispose the page when we leave, no memory leak
    Scaling in _buildPageItem method using Flutter API Matrix4
    Checking for rounded floor value
    Getting scaleFactor as we scroll left-right
     else if condition for the next slide
    Passing currentScale  value to Matrix (x,y,z)
    Wrapping Stack around Transform Widget
    Positioning on the screen Y factor for height should be changed
    Refering to the left slide else if
    Immideatly scaling for the right in else check condition

--- lib/home/farms_page_body.dart
+++ lib/home/farms_page_body.dart
@@ -13,6 +13,25 @@ class FarmsPageBody extends StatefulWidget {
 
 class _FarmsPageBodyState extends State<FarmsPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+  //Overriding init state method
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,110 +47,137 @@ class _FarmsPageBodyState extends State<FarmsPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-            height: 220,
-            margin: const EdgeInsets.only(left: 10, right: 10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: index.isEven
-                    ? const Color(0xFF69c5df)
-                    : const Color(0xFF9294cc),
-                image: const DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/images/huerto1.png')))),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30), color: Colors.white),
+    Matrix4 matrix = Matrix4.identity();
+    if (index == _currentPageValue.floor()) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currentTransPosititon = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currentTransPosititon, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currentScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currentTransPosititon = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currentTransPosititon, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currentScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currentTransPosititon = _height * (1 - currentScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, currentTransPosititon, 0);
+    } else {
+      var currentScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currentScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 0);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+              height: 220,
+              margin: const EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: index.isEven
+                      ? const Color(0xFF69c5df)
+                      : const Color(0xFF9294cc),
+                  image: const DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/images/huerto1.png')))),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        BigText(
-                          text: 'Rancho de Makiko',
-                          size: 18,
-                        ),
-                        const IconAndText(
-                          icon: Icons.location_on,
-                          iconColor: Colors.blueAccent,
-                          text: '2.4 km',
-                        ),
-                      ],
+              height: 120,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              child: Container(
+                padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          BigText(
+                            text: 'Rancho de Makiko',
+                            size: 18,
+                          ),
+                          const IconAndText(
+                            icon: Icons.location_on,
+                            iconColor: Colors.blueAccent,
+                            text: '2.4 km',
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Wrap(
-                          children: List.generate(
-                              5,
-                              (index) => const Icon(
-                                    Icons.star,
-                                    color: AppColors.mainColor,
-                                    size: 15,
-                                  )),
+                    const SizedBox(height: 10),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Wrap(
+                            children: List.generate(
+                                5,
+                                (index) => const Icon(
+                                      Icons.star,
+                                      color: AppColors.mainColor,
+                                      size: 15,
+                                    )),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          SmallText(
+                            text: '4.8',
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          SmallText(
+                            text: '1234',
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          SmallText(text: 'comentarios')
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: const [
+                        IconAndText(
+                          icon: Icons.compost,
+                          iconColor: AppColors.iconColor2,
+                          text: 'Tierra de oja',
                         ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        SmallText(
-                          text: '4.8',
-                        ),
-                        const SizedBox(
+                        SizedBox(
                           width: 5,
                         ),
-                        SmallText(
-                          text: '1234',
+                        SizedBox(
+                          width: 15,
                         ),
-                        const SizedBox(
-                          width: 5,
+                        IconAndText(
+                          icon: Icons.dashboard,
+                          iconColor: AppColors.mainColor,
+                          text: '2.4 m2',
                         ),
-                        SmallText(text: 'comentarios')
                       ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: const [
-                      IconAndText(
-                        icon: Icons.compost,
-                        iconColor: AppColors.iconColor2,
-                        text: 'Tierra de oja',
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      IconAndText(
-                        icon: Icons.dashboard,
-                        iconColor: AppColors.mainColor,
-                        text: '2.4 m2',
-                      ),
-                    ],
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
