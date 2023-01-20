010f3750cb089206c295ed0b161e28f93202075f
GitRockie <elromankos@gmail.com>
2023-01-20 11:26:29 +0100

    Backend Starts Api Client
    init create in dependencies
    creating data and variables for initialization
    api client create, lately initializing token there
    Creating the Constructor and its body from GetX management system: baseUrl and timeout setting the request on 30
    Creatin private _mainHeaders for storing data locally converting to Map

---
+++ lib/data/api/api_client.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class ApiClient extends GetConnect implements GetxService {
+  late String token;
+  final String appBaseUrl;
+  late Map<String, String> _mainHeaders;
+
+  ApiClient({required this.token, required this.appBaseUrl}) {
+    baseUrl = appBaseUrl;
+    timeout = const Duration(seconds: 30);
+    _mainHeaders = {
+      'Content-type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token',
+    };
+  }
+}
